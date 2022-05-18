416e71c21750f563d9453c5fce54ffa6d3e90b06
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-05-18 17:44:10 +0200

    Creating an endpoint for email verification

---
+++ controllers/users/verifyEmail.js
@@ -0,0 +1,23 @@
+const { User } = require('../../models/user');
+const { createError } = require('../../helpers');
+
+const verifyEmail = async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+
+  if (!user) {
+    throw createError(404, 'User not found');
+  }
+
+  await User.findByIdAndUpdate(user._id, {
+    verificationToken: null,
+    verify: true,
+  });
+
+  res.json({
+    code: 200,
+    message: 'Verification successful',
+  });
+};
+
+module.exports = verifyEmail;
